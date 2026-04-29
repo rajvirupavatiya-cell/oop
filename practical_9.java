8fe135b4a76413ebc1a84a99c62ce22df433805e
rajvirupavatiya-cell <rajvirupavatiya@gmail.com>
2026-04-29 14:09:29 +0200

    Create practical_9.java

---
+++ practical_9.java
@@ -0,0 +1,52 @@
+public class Rectangle {
+    // Data fields
+    double width;
+    double height;
+
+    // Constructor
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    // Method to calculate area
+    public double getArea() {
+        return width * height;
+    }
+
+    // Method to calculate perimeter
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // Create two Rectangle objects
+        Rectangle r1 = new Rectangle(4, 40);
+        Rectangle r2 = new Rectangle(3.5, 35.9);
+
+        // Display details of Rectangle 1
+        System.out.println("Rectangle 1:");
+        System.out.println("Width = " + r1.width);
+        System.out.println("Height = " + r1.height);
+        System.out.println("Area = " + r1.getArea());
+        System.out.println("Perimeter = " + r1.getPerimeter());
+
+        // Display details of Rectangle 2
+        System.out.println("\nRectangle 2:");
+        System.out.println("Width = " + r2.width);
+        System.out.println("Height = " + r2.height);
+        System.out.println("Area = " + r2.getArea());
+        System.out.println("Perimeter = " + r2.getPerimeter());
+
+        // Compare areas
+        System.out.println("\nComparison Result:");
+        if (r1.getArea() > r2.getArea()) {
+            System.out.println("Rectangle 1 has a larger area.");
+        } else if (r1.getArea() < r2.getArea()) {
+            System.out.println("Rectangle 2 has a larger area.");
+        } else {
+            System.out.println("Both rectangles have equal area.");
+        }
+    }
+}
